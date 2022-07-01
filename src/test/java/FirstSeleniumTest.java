c8ae653411f956caf32bb4dbf79312837a26dc70
liva-nazarenko <105019479+liva-nazarenko@users.noreply.github.com>
2022-07-02 00:03:09 +0300

    edit

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -2,6 +2,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 public class FirstSeleniumTest {
 
